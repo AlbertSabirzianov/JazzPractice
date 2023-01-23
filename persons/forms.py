a3bde23178c20cd6114df4a35491cfe08c6762ef
AlbertSabirzianov <Albertuno@mail.ru>
2023-01-23 18:49:33 +0300

    Add widgets in forms

--- persons/forms.py
+++ persons/forms.py
@@ -4,14 +4,25 @@ from django.contrib.auth.models import User
 
 
 class Registration(UserCreationForm):
-    username = forms.CharField(label='Логин',
-                               widget=forms.TextInput(attrs={'class': 'form-input'}))
-    email = forms.CharField(label='Email',
-                            widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Пароль',
-                                widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Повтор пароля',
-                                widget=forms.TextInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='',
+                               widget=forms.TextInput(attrs={'class': 'form-control',
+                                                             'id': "floatingInput",
+                                                             'placeholder': 'Логин'}))
+    email = forms.CharField(label='',
+                            widget=forms.TextInput(attrs={'class': 'form-control',
+                                                          'id': "floatingInput",
+                                                          'type': 'email',
+                                                          'placeholder': 'Email'}))
+    password1 = forms.CharField(label='',
+                                widget=forms.TextInput(attrs={'class': 'form-control',
+                                                              'id': "floatingPassword",
+                                                              'type': 'password',
+                                                              'placeholder': 'Пароль'}))
+    password2 = forms.CharField(label='',
+                                widget=forms.TextInput(attrs={'class': 'form-control',
+                                                              'id': "floatingPassword",
+                                                              'type': 'password',
+                                                              'placeholder': 'Повторите Пароль'}))
 
     class Meta:
         model = User
@@ -19,7 +30,12 @@ class Registration(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Логин',
-                               widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Пароль',
-                               widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='',
+                               widget=forms.TextInput(attrs={'class': 'form-control',
+                                                             'id': "floatingInput",
+                                                             'placeholder': 'Логин'}))
+    password = forms.CharField(label='',
+                               widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                 'id': "floatingPassword",
+                                                                 'type': 'password',
+                                                                 'placeholder': 'Пароль'}))
